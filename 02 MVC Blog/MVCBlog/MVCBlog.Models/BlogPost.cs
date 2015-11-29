ebbaf7ed2cbae693bd112e44ed7ec8b9f35b7567
Jim Shaw <jimxshaw@gmail.com>
2015-11-29 14:59:41 -0500

    modified home/index view

--- 02 MVC Blog/MVCBlog/MVCBlog.Models/BlogPost.cs
+++ 02 MVC Blog/MVCBlog/MVCBlog.Models/BlogPost.cs
@@ -9,9 +9,9 @@ namespace MVCBlog.Models
     public class BlogPost
     {
         public int BlogPostID { get; set; }
-       // public string Title { get; set; }
-        public string Text { get; set; }
-        public DateTime Date { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime PostDate { get; set; }
         public HashTag HashTag { get; set; }
         public int Approved { get; set; }
         public User User { get; set; }//has userlevel in it
