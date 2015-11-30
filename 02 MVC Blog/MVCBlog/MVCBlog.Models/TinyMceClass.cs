3f34c648899996e0110aee0be6d608379ab781d9
unknown <domandbillsmom@gmail.com>
2015-11-30 14:54:04 -0500

    TinyMCE with views

---
+++ 02 MVC Blog/MVCBlog/MVCBlog.Models/TinyMceClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace MVCBlog.Models
+{
+    //Class to store TinyMce values
+    public class TinyMceClass
+    {
+        //Attribute allows HTML Content to be sent up.
+        [AllowHtml]
+        public string HtmlContent { get; set; }
+
+
+
+
+
+
+        //public TinyMceClass()
+        //{
+
+        //}
+
+    }
+}
---
+++ 02 MVC Blog/MVCBlog/MVCBlog.UI/Scripts/tinymce/myscript.js
@@ -0,0 +1,5 @@
+﻿tinyMCE.init({
+    force_p_newlines: false,
+    force_root_block: ""
+
+});
