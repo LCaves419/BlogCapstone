91c55c1c512400518ab6438373d10a99e43e1e47
unknown <domandbillsmom@gmail.com>
2015-12-01 15:25:37 -0500

    added some comments from codealong in  class for MVC project with identity example

--- 02 MVC Blog/MVCBlog/MVCBlog.UI/Models/IdentityModels.cs
+++ 02 MVC Blog/MVCBlog/MVCBlog.UI/Models/IdentityModels.cs
@@ -7,8 +7,13 @@ using Microsoft.AspNet.Identity.EntityFramework;
 namespace MVCBlog.UI.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    //IdentityUser is pre-difined in .NET
     public class ApplicationUser : IdentityUser
     {
+        //added in codealong
+        //public string FirstName { get; set; }
+        //public string LastName { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
--- 02 MVC Blog/MVCBlog/MVCBlog.UI/Startup.cs
+++ 02 MVC Blog/MVCBlog/MVCBlog.UI/Startup.cs
@@ -4,6 +4,7 @@ using Owin;
 [assembly: OwinStartupAttribute(typeof(MVCBlog.UI.Startup))]
 namespace MVCBlog.UI
 {
+    //LJ- this partial is paert of Startup.Auth.cs
     public partial class Startup
     {
         public void Configuration(IAppBuilder app)
