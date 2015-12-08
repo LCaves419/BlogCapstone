16eab391cb6f961e512ee699b7573728d5b31ef7
Jim Shaw <jimxshaw@gmail.com>
2015-12-08 15:33:29 -0500

    unit tests for create blogpost & static page repo methods

--- 02 MVC Blog/MVCBlog/MVCBlog.Tests/MVCRepoTests.cs
+++ 02 MVC Blog/MVCBlog/MVCBlog.Tests/MVCRepoTests.cs
@@ -4,8 +4,10 @@ using System.Configuration;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Net.Configuration;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using Dapper;
 using MVCBlog.BLL;
 using MVCBlog.Data;
 using MVCBlog.Data.Config;
@@ -17,6 +19,50 @@ namespace MVCBlog.Tests
     [TestFixture]
     public class MVCRepoTests
     {
+        [Test]
+        public void CreateBlogPostDB()
+        {
+            var blogPost = new BlogPost()
+            {
+                BlogPostID = 0,
+                Body = "",
+                Categories = new List<Category>(),
+                Category = new Category()
+                {
+                    CategoryID = 2,
+                    CategoryName = "Daily Picks"
+                },
+                HashTag = new HashTag()
+                {
+                    HashTagID = 1,
+                    HashTagName = "#savings"
+                },
+                HashTags = new List<HashTag>(),
+                Mce = new TinyMceClass()
+                {
+                    Body = "Unit Testing for CreateBlogPost"
+                },
+                PostDate = DateTime.Today,
+                Status = 2,
+                tags = new List<string>()
+                {
+                    "#goodfood",
+                    "#bestdeals"
+                }
+            };
+
+            var repo = new MVCBlogRepo();
+
+            repo.CreateBlogPostDB(blogPost);
+
+            var cn = new SqlConnection(Settings.ConnectionString);
+
+            var expected = cn.Query<int>("SELECT COUNT(BlogPostID) FROM BlogPosts").FirstOrDefault();
+
+            Assert.AreEqual(expected, 9);
+
+        }
+
         [Test]
         public void GetAllBlogPosts_CheckCount()
         {
@@ -129,6 +175,33 @@ namespace MVCBlog.Tests
 
             var repo = new MVCBlogRepo();
             Assert.AreEqual(expected, repo.GetAllArchivedBlogPosts().Count());
+        }
+
+        [Test]
+        public void CreateStaticPageDB()
+        {
+            
+            var staticPage = new StaticPage()
+            {
+                Date = DateTime.Today,
+                Mce = new TinyMceClass()
+                {
+                    Body = "Unit Test for CreateStaticPageDB method"
+                },
+                StaticPageID = 0,
+                Status = 2,
+                Title = "Testing",
+                UserID = "2598e14d-88d0-46a7-ace9-f323ffe89181"
+            };
+
+            var repo = new MVCBlogRepo();
+            repo.CreateStaticPageDB(staticPage);
+
+            var cn = new SqlConnection(Settings.ConnectionString);
+
+            var expected = cn.Query<int>("SELECT COUNT(StaticPageID) FROM StaticPages").FirstOrDefault();
+
+            Assert.AreEqual(expected, 6);
         }
 
         [Test]
